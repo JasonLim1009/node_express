e1086190c14dfce81f86b09202b955102067efeb
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-08-23 16:41:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//221
+// pwd
+// ls
+// mkdir
+// cd
+// touch
+// start
+// atom .
+// node index.js
+
+
+
+//223
+// const fs = require('fs');
+// fs.copyFileSync('file1.txt', 'file2.txt');
+// ***create file1.txt & file2.txt
+
+
+
+//224
+// var superheroes = require('superheroes');
+// var supervillains = require('supervillains');
+
+// var mySuperherosName = superheroes.random();
+// var mySupervillainsName = supervillains.random();
+
+// console.log(mySuperherosName);
+// console.log(mySupervillainsName);
+
+// npm init
+// npmjs.com
+// npm install superheroes
+// npm install supervillains
+// node index.js
